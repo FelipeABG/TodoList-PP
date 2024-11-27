f7f99d1e24692af52718bda357e4cb9ed431eee3
Felipe Augusto <felipe.baleche2@gmail.com>
2024-11-27 17:15:47 -0300

    aligned all side bar content

--- src/js/entities/project.js
+++ src/js/entities/project.js
@@ -1,5 +1,4 @@
 import { create } from "../utils/node.js";
-import circleIcon from "/src/img/circle.svg";
 
 export default class {
   constructor(name, tasks) {
@@ -10,10 +9,7 @@ export default class {
   intoNode() {
     const projectTitle = create("div")
       .class("title-container")
-      .child(
-        create("img").class("title-img").source(circleIcon).build(),
-        create("p").content(this.name).build(),
-      )
+      .child(create("p").content(this.name).build())
       .build();
 
     if (this.tasks.length == 0) {
