7694e0e21456e1628f57aa51174472489a0c5782
felipe <felipe.baleche2@gmail.com>
2024-12-05 17:22:13 -0300

    finished new project modal design

--- src/js/ui/components/project-modal.js
+++ src/js/ui/components/project-modal.js
@@ -1,9 +1,33 @@
 import { get, create } from "/src/js/utils/fluent";
 
 export default class {
-  static render() {}
+  static render() {
+    get(".new-project-container").child(
+      create("div").class("project-input-container").build(),
+      create("div").class("project-button-container").build(),
+    );
+
+    get(".project-input-container").child(this.#renderInput());
+    get(".project-button-container").child(this.#renderButton());
+  }
 
   static openProjectModal() {
     document.querySelector(".new-project-dialog").showModal();
+  }
+
+  static #renderInput() {
+    return create("input")
+      .type("text")
+      .placeholder("Project name")
+      .class("project-input")
+      .build();
+  }
+
+  static #renderButton() {
+    return create("input")
+      .type("submit")
+      .value("Create Project")
+      .class("project-submit")
+      .build();
   }
 }
